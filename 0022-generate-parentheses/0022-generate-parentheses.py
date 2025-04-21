66804cc79dd145d822d9efee8668c74b86978a60
Rahul Patil <72222863+rahul-patill@users.noreply.github.com>
2025-04-20 20:45:31 -0500

    Time: 0 ms (100%), Space: 17.9 MB (70.34%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def generateParenthesis(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        def Generate_Parentheses(sentence, left, right):
+            if len(sentence) == n * 2:
+                l.append(sentence)
+                return
+            if left < n:
+                Generate_Parentheses(sentence + '(', left + 1, right)
+            if right < left:
+                Generate_Parentheses(sentence + ')', left, right + 1)
+
+        l = []
+        Generate_Parentheses('',0,0)
+        return l
