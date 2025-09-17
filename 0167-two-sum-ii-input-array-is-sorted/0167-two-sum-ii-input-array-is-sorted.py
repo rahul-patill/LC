375b658e9159fbdf4c98b695cb47f87804caa206
Rahul Patil <72222863+rahul-patill@users.noreply.github.com>
2025-09-16 19:04:52 -0500

    Time: 7 ms (23.16%), Space: 18.5 MB (90.15%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -1,16 +1,14 @@
 class Solution:
-    def twoSum(self, num: List[int], target: int) -> List[int]:
-        
-        # solution using two pointer
-        l, r = 0, len(num) - 1
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        l = 0
+        r = len(nums) - 1
 
-        while l<r:
-            curSum = num[l] + num[r]
+        while l < r:
 
-            if curSum > target:
-                r-=1
-            elif curSum < target:
-                l+=1
-            else:
+            if nums[l] + nums[r] == target:
                 return [l+1, r+1]
-            
+
+            elif nums[l] + nums[r] > target:
+                r = r - 1
+            elif nums[l] + nums[r] < target:
+                l += 1
