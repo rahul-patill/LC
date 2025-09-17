5dffc0d1ae64b7d51d8632e671fee6f32fd2b91c
Rahul Patil <72222863+rahul-patill@users.noreply.github.com>
2025-09-16 20:38:50 -0500

    Time: 2184 ms (5%), Space: 20.4 MB (92.53%) - LeetHub

---
+++ 0015-3sum/0015-3sum.py
@@ -0,0 +1,25 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        ans = set() #storing the duplicate triplet
+        result = []
+
+        for i in range(0, len(nums)-2):
+            l = i+1
+            r = len(nums)-1
+
+            while l<r:
+                if nums[i]+nums[l]+nums[r] == 0:
+                    ans.add((nums[i],nums[l],nums[r]))
+                    l+=1
+                    r-=1
+                elif nums[i]+nums[l]+nums[r] < 0:
+                    l+=1
+                elif nums[i]+nums[l]+nums[r] > 0:
+                    r-=1
+        
+        for items in ans:
+            print(items)
+            result.append(items)
+        
+        return result
