5ab52b58afd2627667a56fa7ef4425b0a8651db3
Rahul Patil <72222863+rahul-patill@users.noreply.github.com>
2026-01-08 23:30:57 -0600

    Time: 0 ms (100%), Space: 19.8 MB (14.42%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.py
+++ 0162-find-peak-element/0162-find-peak-element.py
@@ -6,11 +6,11 @@ class Solution:
         if(len(nums) == 1 ):
             return 0
 
-        if(len(nums) == 2):
-            if(nums[0] > nums[1]):
-                return 0
-            else:
-                return 1
+        # if(len(nums) == 2):
+        #     if(nums[0] > nums[1]):
+        #         return 0
+        #     else:
+        #         return 1
 
 
 
@@ -39,7 +39,7 @@ class Solution:
                 if (nums[mid] > nums[mid+1]):
                     return 0
                 else:
-                    return 1
+                    return 1 #why this?
             
             # if mid at last
             if (mid == len(nums) -1):
