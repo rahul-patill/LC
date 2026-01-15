c626446400012ff6360af97b0b8381bd7bf0d24b
Rahul Patil <72222863+rahul-patill@users.noreply.github.com>
2026-01-14 23:10:15 -0600

    Time: 22 ms (20.59%), Space: 24.4 MB (5.13%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -2,12 +2,17 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         
         dic = defaultdict(list)
-
+        
+        print("dic : ", dic)
         for word in strs:
             alphaMap = [0] * 26
             for alphabet in word:
                 alphaMap[ord(alphabet) - 97] += 1
             alphaMap = tuple(alphaMap)
+            # tuple is used bcoz dictionary needs a key as tuple.
+            # it cant use list as key, since its mutable
+
+
             dic[alphaMap].append(word)
         
         print(dic.values())
