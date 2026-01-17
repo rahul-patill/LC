8f43af6b7ed4b5e0cd74ade58c9e6327b4b10f2f
Rahul Patil <72222863+rahul-patill@users.noreply.github.com>
2026-01-17 14:14:47 -0600

    Time: 0 ms (100%), Space: 19.6 MB (7.27%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,43 +1,38 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        # first fir nge for nums 2 and store it in dict
-
-
-        # declaring
-        stack = []  # which is a list
-        d = {}  # dictionary
-
-        for e in nums2[::-1]:
-            # if stack is empty for the last element
-            if (stack == []):
-                d[e] = -1
-            
-            
-            # if element if there in stack and element < stack top, so put in dictionary
-            elif ( e < stack[-1] ):
-                d[e] = stack[-1]
-            
-            
-            # if element if there in stack and element < stack top, so pop till 1)it becomes empty or 2)found element > stack top
-            elif (e > stack[-1]):
-                while ( len(stack)>0 and (e > stack[-1])  ) :
-                    stack.pop()
-
-                if  stack == []:
-                    d[e] = -1
-                elif (e < stack[-1]):
-                    d[e] = stack[-1]
-
-            stack.append(e)
-        
-
-        # now put value in list for the nums , using dictionary for nums 2
 
         ans = []
-        for e in nums1:
-            ans.append(d[e])
-        
-        return ans
+        s = []
 
+        for i in range(len(nums2) - 1, -1, -1):
+            if not s:
+                ans.append(-1)
+
+            elif nums2[i] < s[-1]:
+                ans.append(s[-1])
             
+            elif nums2[i] > s[-1]:
+                while len(s) != 0 and nums2[i] > s[-1]:
+                    s.pop(-1)
+                if len(s) == 0:
+                    ans.append(-1)
+                else:
+                    ans.append(s[-1])
 
+            s.append(nums2[i])
+
+        ans.reverse()
+        print(ans)
+
+
+        d = defaultdict(int)
+        for i in range(0, len(nums2)):
+            d[nums2[i]] = ans[i]
+        
+        print(d)
+
+        result = []
+        for i in nums1:
+            result.append(d[i])
+        
+        return result
